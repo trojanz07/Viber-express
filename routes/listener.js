afc2bee563870c352d67243a8181b9dd07584b53
trojanz07 <97985367+trojanz07@users.noreply.github.com>
2022-01-20 23:03:55 +0100

    Update listener.js

--- routes/listener.js
+++ routes/listener.js
@@ -1,9 +1,13 @@
-const chalk = require('chalk');
+const terminal = require('@skylixgh/nitrojs-terminal');
 function listen(app, port) {
     app.listen(port, () => {
-        console.log(chalk.bold.grey('Starting...'))
-        console.log(chalk.bold.grey('Preparing the website...'))
-        console.log(chalk.bold.red(`Listening on port ${port}`))
+        terminal.animate('Starting...');
+        setTimeout(() =>  {
+            terminal.updateAnimation('Preparing the website...')
+            setTimeout(() =>  {
+                terminal.updateAnimation('Listening on port ' + port)
+            },2000)
+        },2000)
     })
 }
 
